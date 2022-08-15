647d4727bea131dc1442dcbac383bce48419e356
Cassie <11220430+git-cassie@user.noreply.gitee.com>
2022-08-15 20:28:07 +0800

    login-style

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view />
+   <!--  <Pageination
+      :total="299"
+      :page-size="10"
+      :current-page="current"
+      @changePageEvent="changePageEvent"
+    /> -->
+  </div>
+</template>
+
+<script>
+// import Pageination from "@/components/Pageination";
+export default {
+  name: "App",
+  components: {
+   // Pageination,
+  },
+  data() {
+    return {
+      current: 1,
+    };
+  },
+  methods: {
+    changePageEvent(val) {
+      this.current = val;
+      console.log(val);
+    },
+  },
+};
+</script>
